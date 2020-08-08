5bac831abc8f0c177b0660b84094f479c5a2058d
unknown <jordan.smta@gmail.com>
2020-08-08 12:51:45 -0700

    First Commit

---
+++ WcfRefactoring.Service/Calc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WcfRefactoring
+{
+    public class Calc : ICalc
+    {
+        //public int Sum(int part1, int part2) => part1 * part2;
+
+        public int Sum(int part1, int part2)
+        {
+            var returnValue = part1 + part2;
+
+            Console.WriteLine($"{part1} + {part2} = {returnValue}");
+
+            return returnValue;
+        }
+    }
+}
